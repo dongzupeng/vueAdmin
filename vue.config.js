2b71361126ae00555a7e69122884211f50bba4f2
dzp <2360591410@qq.com>
2022-11-18 20:01:31 +0800

    feat:配置修改

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-// const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const resolve = (dir) => path.resolve(__dirname, dir);
 
 
@@ -27,17 +27,17 @@ module.exports = {
     if (process.env.NODE_ENV === "production") {
       // 为生产环境修改配置
       config.plugins.push(
-        // new UglifyJsPlugin({
-        //   uglifyOptions: {
-        //     compress: {
-        //       drop_debugger: true,
-        //       drop_console: true, //生产环境自动删除console
-        //     },
-        //     warnings: false,
-        //   },
-        //   sourceMap: false,
-        //   parallel: true, //使用多进程并行运行来提高构建速度。默认并发运行数：os.cpus().length - 1。
-        // })
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            compress: {
+              drop_debugger: true,
+              drop_console: true, //生产环境自动删除console
+            },
+            warnings: false,
+          },
+          sourceMap: false,
+          parallel: true, //使用多进程并行运行来提高构建速度。默认并发运行数：os.cpus().length - 1。
+        })
       );
     }
   },
