57a6223db3e1c5dbd8ab723435aff715a2281a25
dzp <2360591410@qq.com>
2022-11-23 01:49:22 +0800

    fix

--- src/plugins/cdn.js
+++ src/plugins/cdn.js
@@ -1,9 +1,9 @@
 const ElementCDN = "https://unpkg.com/element-ui/lib/index.js";
 const Echarts ="https://cdn.bootcdn.net/ajax/libs/echarts/5.4.0/echarts.min.js";
-const VueCDN = 'https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.min.js';
-const AxiosCDN = 'https://cdn.jsdelivr.net/npm/axios@0.21.0/dist/axios.min.js';
-const VueRouterCDN = 'https://cdn.jsdelivr.net/npm//vue-router@3.2.0/dist/vue-router.min.js';
-const VuexCDN = 'https://cdn.jsdelivr.net/npm/vuex@3.5.1/dist/vuex.min.js';
+const VueCDN = 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.min.js';
+const AxiosCDN = 'https://cdn.jsdelivr.net/npm/axios@1.1.3/dist/axios.min.js';
+const VueRouterCDN = 'https://cdn.jsdelivr.net/npm//vue-router@3/dist/vue-router.min.js';
+const VuexCDN = 'https://cdn.jsdelivr.net/npm/vuex@3.4.1/dist/vuex.min.js';
 const TinymceCDN = 'https://cdn.jsdelivr.net/npm/tinymce-all-in-one@4.9.3/tinymce.min.js';
 const html2canvasCDN = 'https://cdn.jsdelivr.net/npm/html2canvas@1.0.0-rc.7/dist/html2canvas.min.js';
 
