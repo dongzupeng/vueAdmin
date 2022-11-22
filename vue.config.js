9449b6d2a636d54e74a96707acf19e2e50c96e81
dzp <2360591410@qq.com>
2022-11-23 02:37:59 +0800

    fix:cdn链接配置

--- vue.config.js
+++ vue.config.js
@@ -5,20 +5,44 @@ const resolve = (dir) => path.resolve(__dirname, dir);
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const isProd = process.env.NODE_ENV === 'production';
-const { Echarts, AxiosCDN, VuexCDN } = require('./src/plugins/cdn');
+//配置引用cdn的js、css地址
 const cdn = {
-  css: [],
-  js: [Echarts, AxiosCDN, VuexCDN],
-  externals: {
-    echarts: "Echarts",
-    vuex: 'Vuex',
-    axios: 'axios'
-  }
-};
+  // css: [
+  //   'https://unpkg.com/element-ui@2.13.2/lib/theme-chalk/index.css'
+  // ],
+  js: [
+    'https://cdn.bootcdn.net/ajax/libs/vue/2.6.10/vue.min.js',
+    'https://cdn.bootcdn.net/ajax/libs/vue-router/3.0.2/vue-router.min.js',
+    'https://cdn.bootcdn.net/ajax/libs/vuex/3.1.0/vuex.min.js',
+    'https://cdn.bootcdn.net/ajax/libs/axios/0.18.1/axios.min.js',
+    'https://unpkg.com/element-ui@2.13.2/lib/index.js',
+    'https://cdn.bootcdn.net/ajax/libs/echarts/5.0.1/echarts.min.js'
+  ]
+}
+
+const externals = {
+  // 属性名称 vue, 表示遇到 import xxx from 'vue' 这类引入 'vue'的，不去 node_modules 中找，而是去找全局变量 Vue（其他的为VueRouter、Vuex、axios、ELEMENT、echarts，注意全局变量是一个确定的值，不能修改为其他值，修改为其他大小写或者其他值会报错，若有异议可留言）
+  vue: 'Vue',
+  'vue-router': 'VueRouter',
+  vuex: 'Vuex',
+  axios: 'axios',
+  'element-ui': 'ELEMENT',
+  'echarts': 'echarts'
+}
+
 
 module.exports = {
   publicPath: "./",
   chainWebpack: (config) => {
+    //cdn
+    if (isProd) {
+      config.plugin('html').tap(args => {
+        args[0].cdn = cdn
+        return args
+      })
+      //视为一个外部库，而不将它打包进来
+      config.externals(externals)
+    }
     // 配置svg
     config.module
       .rule('svg')
@@ -55,9 +79,6 @@ module.exports = {
       //配置插件
       config.plugins.push(new BundleAnalyzerPlugin());
     }
-  },
-  configureWebpack: {
-    externals: isProd ? cdn.externals : {}
   },
   css: {
     loaderOptions: {
